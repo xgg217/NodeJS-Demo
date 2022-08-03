07d521541f3fc20854be4af1c55d05c74b436188
付祖敏 <1326580471@qq.com>
2022-08-03 21:23:50 +0800

    文件调整

--- 项目实战/3.电影后台/service/src/init.ts
+++
@@ -1,9 +0,0 @@
-import app from './routes/init';
-import { Admin, sequelize } from './models/sync'
-
-
-export {
-  app,
-  Admin,
-  sequelize
-}
--- 项目实战/3.电影后台/service/src/models/db.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('maoyandb', 'root', '081213', {
-  host: 'localhost',
-  dialect: 'mysql' /* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */
-});
-
-export default sequelize;
-
